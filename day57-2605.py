098b95ff0016315f4dea8b7f353052ecab3dca93
maheshwaranandh <126230893+maheshwaranandh@users.noreply.github.com>
2023-12-24 23:40:10 +0530

    Update day57-2605.py

--- day57-2605.py
+++ day57-2605.py
@@ -1,1 +1,6 @@
-https://leetcode.com/problems/form-smallest-number-from-two-digit-arrays/description/
+class Solution:
+    def minNumber(self, nums1: List[int], nums2: List[int]) -> int:
+        for i in sorted(nums1):
+            if i in nums2:
+                return i
+        return min(min(nums1),min(nums2))*10+max(min(nums1),min(nums2))
